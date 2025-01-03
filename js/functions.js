09109ed889e526bdbe941ae86836bdb5f59eeed2
drJeckyll <drjeckyll@jeckyll.net>
2025-01-03 20:51:39 +0200

    fix OX - broken info

--- js/functions.js
+++ js/functions.js
@@ -524,7 +524,7 @@ function parsePacket(hex, check_confirm = 0)
 		$('.settings .gear_values .vcontent .content').html('');
 		for (var t = 0; t < parseInt(info['TOTAL_CNT']); t++) {
 		    var r = parseInt(info['TOTAL_CNT']) - t;
-		    $('.settings .gear_values .vcontent .content').append('<div class="gear" gear="' + (t + 1) + '"><div class="button minus">-</div><input type="text" value="' + parseInt(info['H_GEA[' + (t + 1) + ']']) + '"><div class="button plus gear' + r + '">+</div><div class="button set">Set</div></div>');
+		    $('.settings .gear_values .vcontent .content').append('<div class="gear" gear="' + (t + 1) + '"><div class="button minus">-</div><input type="text" value="' + parseInt(info['GEARS[' + (t + 1) + ']']) + '"><div class="button plus gear' + r + '">+</div><div class="button set">Set</div></div>');
 		}
 	    } else
 	    if (device_type == "EDS TX") {
