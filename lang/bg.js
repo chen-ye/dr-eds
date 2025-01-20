e6db9cd055e16b79ebd94184c25f1c762aad729a
drJeckyll <drjeckyll@jeckyll.net>
2025-01-20 13:10:16 +0200

    full BG translation
    Changelog: feature

--- lang/bg.js
+++ lang/bg.js
@@ -1,5 +1,92 @@
 
+lang['L_GET_KEY'] = "Получаване на ключ";
 lang['L_GET_INFO'] = "Извличане на информация";
+
+lang['L_UP_SHIFT'] = "Предавка нагоре";
+lang['L_DOWN_SHIFT'] = "Предавка надолу";
+lang['L_FRONT_SHIFT'] = "Превключване отпред";
+lang['L_FRONT_UP_SHIFT'] = "Отпред предавка нагоре";
+lang['L_FRONT_DOWN_SHIFT'] = "Отпред предавка надолу";
+lang['L_UP_MICRO_SHIFT'] = "Микро превключване нагоре";
+lang['L_DOWN_MICRO_SHIFT'] = "Микро превключване надолу";
+lang['L_FRONT_UP_MICRO_SHIFT'] = "Отпред микро превключване нагоре";
+lang['L_FRONT_DOWN_MICRO_SHIFT'] = "Отпред микро превключване надолу";
+
+lang['L_NUMBER_OF_GEARS_CHANGED'] = "Броя на скоростите беше променен";
+
+lang['L_GEAR_VALUE_UPDATED'] = "Стойността беше променена";
+lang['L_FRONT_GEAR_VALUE_UPDATED'] = "Стойността беше променена";
+
+lang['L_GET_RD_PROTECTION'] = "Получаване на информация за защита на ЗД";
+lang['L_GET_FD_INFORMATION'] = "Получаване на информация от ПД";
+lang['L_GET_SLEEP_STATUS'] = "Получаване на статус за заспиване";
+
+lang['L_UPDATE_VALUE'] = "Промени стойност";
+lang['L_UPDATE_LIMIT'] = "Промени лимит";
+lang['L_UPDATE_ALL_VALUES'] = "Промени всички стойности";
+lang['L_UPDATE_ALL_LIMITS'] = "Промени всички лимити";
+
+lang['L_FD_SMALL'] = "малък";
+lang['L_FD_BIG'] = "голям";
+lang['L_UP'] = "Нагоре";
+lang['L_DOWN'] = "Надолу";
+lang['L_FRONT'] = "Отпред";
+lang['L_SET'] = "Задай";
+lang['L_SET_ALL'] = "Задай всички";
+
+lang['L_COMMAND_FAILED'] = 'Неуспешна команда';
 
 lang['L_NORMAL_BUTTONS'] = "Нормални бутон";
 lang['L_REVERSED_BUTTONS'] = "Обърнати бутони";
+lang['L_SET_BUTTON_COMMANDS'] = "Запис на настройките за бутоните";
+
+lang['L_PRESET_LOADED'] = 'Стойностите бяха заредени от шаблон "%s"<br />Натиснете "Задай всички" за да ги изпратите към ЗД';
+lang['L_PRESET_CONFIRM_DELETE'] = 'Да изтрия ли шаблон "%s"?';
+lang['L_PRESET_DELETED'] = 'Шаблон "%s" беше изтрит';
+lang['L_PROMPT_NAME_PRESET'] = "Въведете име на шаблона (само букви и цифри):";
+lang['L_PROMPT_SAVE_PRESET'] = 'Експортирай настройките, стойностите и шаблоните във файл "%s"?';
+
+lang['L_EXPORT_SUCCESS'] = "Настройките и стойностите бяха запазени";
+lang['L_IMPORT_SUCCESS'] = "Настройките и стойностите бяха възстановени";
+
+lang['L_ERROR_INVALID_NUMBER_OF_GEARS'] = "Избрания брой скорости е<br />по-малък от текущата скорост!<br /><br />Преминете на скорост по-малка<br />или равна на общия брой скорости.";
+lang['L_ERROR_READING_FILE'] = "Грешка при четене на файла";
+lang['L_ERROR_CURRENT_VALUES_NOT_INPORTED'] = "Текущите стойности не са импортирани - различно устройство";
+
+lang['L_RACE'] = "Състезателен";
+lang['L_WARNING_RACE_MODE'] = "Състезателния режим използва повече батерия - продължи?";
+lang['L_RACE_MODE'] = "%s състезателен режим";
+lang["L_RACE_MODE_ON"] = "Включен";
+lang['L_RACE_MODE_OFF'] = 'Изключен';
+
+lang['L_SLEEP'] = "Заспиване";
+lang['L_WARNING_SLEEP_MODE'] = "Изключването на заспиването използва повече батерия - продължи?";
+lang['L_SLEP_MODE'] = "%s заспиване";
+lang['L_SLEP_MODE_ON'] = "Включено";
+lang["L_SLEP_MODE_OFF"] = "Изключено";
+
+lang['L_RD_PROTECT'] = "Защита на ЗД";
+lang['L_WARNING_RD_PROTECT'] = "Изключването на защитата на ЗД може да го повреди - продължи?";
+lang['L_RD_PROTECT_MODE'] = "Защитата на ЗД беше %s";
+lang['L_RD_PROTECT_ON'] = "включена";
+lang['L_RD_PROTECT_OFF'] = "изключена";
+
+lang['L_CONFIRM_SHUTDOWN'] = "Изключи";
+lang['L_SHUTDOWN'] = "Устройството беше изключено";
+
+lang['L_DICSONNECTED'] = "Връзката е прекъсната";
+
+lang['L_COPIED_TO_CLIPBOARD'] = "Копирано в клипборда";
+
+lang['L_SCAN'] = "Намери EDS";
+
+lang['L_RD'] = "ПД";
+lang['L_FD'] = "ЗД";
+lang['L_SHIFTER'] = "Shifter";
+lang['L_SHIFTER_LEFT'] = "Л";
+lang['L_SHIFTER_RIGHT'] = "Д";
+
+lang['L_SAVE_AS'] = "Запиши като";
+
+lang['L_NO_FD_CONNECTED'] = "Няма връзка с ПД - включете и изключете магнитния кабел за зареждане в ПД, докато не се появи синя мигаща светлина отстрани и след това за кратко зелена светлина. Ако стойностите на ПД не се появят до 3-5 секунди щракнете";
+lang['L_NO_FD_CONNECTED_HERE'] = "тук";
