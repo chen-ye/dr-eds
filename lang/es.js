5129b0fb0399a49f88d9d84e5fd673903cf9d343
drJeckyll <drjeckyll@jeckyll.net>
2025-01-26 18:37:48 +0200

    Spanish translation by Jesus Gomez
    Changelog: language

---
+++ lang/es.js
@@ -0,0 +1,94 @@
+
+lang['L_GET_KEY'] = "Obteniendo Clave";
+lang['L_GET_INFO'] = "Obteniendo información";
+
+lang['L_UP_SHIFT'] = "Subir Cambio";
+lang['L_DOWN_SHIFT'] = "Bajar Cambio";
+lang['L_FRONT_SHIFT'] = "Cambio Frontal";
+lang['L_FRONT_UP_SHIFT'] = "Subir Cambio Frontal";
+lang['L_FRONT_DOWN_SHIFT'] = "Bajar Cambio Desviador";
+lang['L_UP_MICRO_SHIFT'] = "Subir Cambio fino";
+lang['L_DOWN_MICRO_SHIFT'] = "Bajar Cambio fino";
+lang['L_FRONT_UP_MICRO_SHIFT'] = "Subir Cambio fino Desviador";
+lang['L_FRONT_DOWN_MICRO_SHIFT'] = "Bajar Cambio fino Frontal";
+
+lang['L_NUMBER_OF_GEARS_CHANGED'] = "Número de marchas cambiadas";
+
+lang['L_GEAR_VALUE_UPDATED'] = "Valor del engranaje actualizado";
+lang['L_FRONT_GEAR_VALUE_UPDATED'] = "Valor del engranaje delantero actualizado";
+
+lang['L_GET_RD_PROTECTION'] = "Entrar en Protección RD";
+lang['L_GET_FD_INFORMATION'] = "Entrar en Información FD";
+lang['L_GET_SLEEP_STATUS'] = "Entrar en Estado de Sueño";
+
+lang['L_UPDATE_VALUE'] = "Actualizar Valor";
+lang['L_UPDATE_LIMIT'] = "Actualizar límites";
+lang['L_UPDATE_ALL_VALUES'] = "Actualizar Todos los Valores";
+lang['L_UPDATE_ALL_LIMITS'] = "Actualizar Todos los Límites";
+
+lang['L_FD_SMALL'] = "Menor";
+lang['L_FD_BIG'] = "Alcalde";
+lang['L_UP'] = "Subir";
+lang['L_DOWN'] = "Bajar";
+lang['L_FRONT'] = "Desviador";
+lang['L_SET'] = "Ajustar";
+lang['L_SET_ALL'] = "Ajustar Todos";
+
+lang['L_COMMAND_FAILED'] = 'Comando fallido';
+
+lang['L_NORMAL_BUTTONS'] = "Botones normales";
+lang['L_REVERSED_BUTTONS'] = "Botones invertidos";
+
+lang['L_SET_BUTTON_COMMANDS'] = "Configurar comandos de botón";
+
+lang['L_PRESET_LOADED'] = 'Valores de marcha cargados desde preset"%s"<br />Haga clic en «Ajustar Todos» para cargar a continuación a RD';
+lang['L_PRESET_CONFIRM_DELETE'] = '¿Eliminar el valor predeterminado "%s"?';
+lang['L_PRESET_DELETED'] = 'Predeterminado "%s" eliminado';
+lang['L_PROMPT_NAME_PRESET'] = "Ingrese el nombre del valor preestablecido de los engranajes (solo letras y números):";
+lang['L_PROMPT_SAVE_PRESET'] = '¿Exportar configuraciones, valores y valores preestablecidos al archivo "%s"?';
+
+lang['L_EXPORT_SUCCESS' ] = "Configuración, valores y ajustes preestablecidos guardados";
+lang['L_IMPORT_SUCCESS'] = "Configuración, valores y ajustes preestablecidos restaurados";
+
+lang['L_ERROR_INVALID_NUMBER_OF_GEARS'] = "El número total de marchas seleccionado es<br />menor que la marcha actual !<br /><br />Por favor, cambie a una marcha menor o igual al número total de marchas.";
+lang['L_ERROR_READING_FILE'] = "Error al leer el archivo";
+lang['L_ERROR_CURRENT_VALUES_NOT_INPORTED'] = "Valores actuales no importados - dispositivo diferente";
+
+lang['L_RACE'] = "Carrera";
+lang['L_WARNING_RACE_MODE'] = "El modo carrera agotará la batería del RD más rápido - ¿desea continuar?";
+lang[ 'L_RACE_MODE'] = "Modo de carrera %s";
+lang["L_RACE_MODE_ON"] = "Habilitar";
+lang['L_RACE_MODE_OFF'] = 'Deshabilitar';
+
+lang['L_SLEEP'] = "Suspender";
+lang['L_WARNING_SLEEP_MODE' ] = "Deshabilitar el modo de suspensión agotará la batería del RD más rápido. ¿Continuar?";
+lang['L_SLEP_MODE'] = "%s modo de suspensión";
+lang['L_SLEP_MODE_ON'] = "Habilitar";
+lang["L_SLEP_MODE_OFF"] = "Deshabilitar";
+
+lang['L_RD_PROTECT'] = "Protección de RD";
+lang['L_WARNING_RD_PROTECT'] = "Deshabilitar la protección de RD puede dañar el tren de transmisión - continuar ?";
+lang['L_RD_PROTECT_MODE'] = "Protección de RD %s";
+lang['L_RD_PROTECT_ON'] = "Habilitar";
+lang['L_RD_PROTECT_OFF'] = "Deshabilitar";
+
+lang['L_CONFIRM_SHUTDOWN'] = "Apagado";
+lang['L_SHUTDOWN'] = "Apagado del dispositivo";
+
+lang['L_CONNECTED'] = "Conectado a";
+lang['L_DICSONNECTED'] = "Desconectado";
+
+lang['L_COPIED_TO_CLIPBOARD'] = "Copiado al portapapeles";
+
+lang['L_SCAN'] = "Buscar EDS";
+
+lang['L_RD'] = "RD";
+lang[ 'L_FD'] = "FD";
+lang['L_SHIFTER'] = "Cambio";
+lang['L_SHIFTER_LEFT'] = "L";
+lang['L_SHIFTER_RIGHT'] = "R";
+
+lang['L_SAVE_AS'] = " Guardar como";
+
+lang['L_NO_FD_CONNECTED'] = "Sin conexión al FD: conecte y desconecte el cargador magnético al FD hasta que vea que la luz azul parpadea en el costado del FD y luego la luz verde parpadea. Si los valores del FD no se muestran después de 3 a 5 segundos, haga clic en ";
+lang['L_NO_FD_CONNECTED_HERE'] = "aquí";
